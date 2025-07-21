93cea64bcda5d57a48730ba38d1e075984151593
Nauman Riaz Chaudhry <nauman.riaz@gmail.com>
2025-07-21 13:54:38 +0100

    BETS Website updated

--- src/components/footers/FooterOne.jsx
+++ src/components/footers/FooterOne.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import footerLogo from "../../assets/img/footer_logo.svg";
+import footerLogo from "../../assets/img/logo.svg";
 
 export const FooterOne = () => {
   return (
