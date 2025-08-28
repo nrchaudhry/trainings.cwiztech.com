adfe113f0d70928225c83936e653274d4f8bbc7b
Nauman Riaz Chaudhry <nauman.riaz@gmail.com>
2025-08-29 00:03:36 +0500

    CWIZTECH Trainings Website updated

--- push.sh
+++ push.sh
@@ -1,10 +1,10 @@
-echo "Pull BETS Website"
+echo "Pull CWIZTECH Trainings Website"
 git pull
 
 echo "Update Changes"
 git add .
 echo "Git Added"
-git commit -m "BETS Website updated"
+git commit -m "CWIZTECH Trainings Website updated"
 echo "Git Commit"
 git push
 echo "Git Updated"
